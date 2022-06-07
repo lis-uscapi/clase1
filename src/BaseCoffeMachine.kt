ef735af513b32da346d36720731807a0fb2b5cfe
unknown <lis.uscapi@gmail.com>
2022-06-07 18:01:45 -0500

    [topic] - abstraction

--- src/BaseCoffeMachine.kt
+++ src/BaseCoffeMachine.kt
@@ -1,16 +1,30 @@
-//Abrir clase
-open class BaseCoffeMachine (
+//Abrir clase :open class BaseCoffeMachine
+//abstraer la clase
+abstract class BaseCoffeMachine (
     //precio
     private val price:Double,
     //color
     private val color:String
 ){
-    //private=al tener el private no se puede tener acceso a la funcion(datos)
-    //private fun makeCoffe(){}
+    abstract val brand:String
+    abstract fun makeCoffe(type:String):String
 
-     fun makeCoffe(){
-        println("Here is your coffe")
+//    aperturar
+     open fun machineInfo():String{
+        return "coffe Machine Details:\n" +
+                "Brand: $brand\n" +
+                "Color: $color\n" +
+                "Price: $price\n"
     }
 
-
 }
+
+    //private=al tener el private no se puede tener acceso a la funcion(datos)
+    //private fun makeCoffe(){}
+        /*
+        PrimiumCoffeMachine
+             fun makeCoffe(){
+                println("Here is your coffe")
+            }
+        */
+
--- src/PrimiumCoffeMachine.kt
+++ src/PrimiumCoffeMachine.kt
@@ -4,7 +4,22 @@ class PrimiumCoffeMachine (
     private val color:String
         ) : BaseCoffeMachine(price ,color){
 
+    override val brand: String
+        get() ="Brand XY"
+
+    override fun makeCoffe(type: String): String {
+        return "Your $type is Ready"
+    }
+
+    override fun machineInfo(): String {
+        return super.machineInfo()
+    }
+
+
+}
+            /*
+            BaseCoffeMachine
             fun makeCapuchino(){
                 println("here is your capuccino")
             }
-}
+            */
